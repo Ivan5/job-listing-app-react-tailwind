c22f36e04cbb4bd6db2b58e42824ddac38467558
Ivan5 <converse_ivan@hotmail.com>
2020-04-14 15:25:16 -0500

    Make filter by tags

--- src/App.js
+++ src/App.js
@@ -4,20 +4,66 @@ import JobBoardComponent from "./components/JobBoardComponent";
 
 function App() {
   const [jobs, setJobs] = useState([]);
+  const [filters, setFilters] = useState([]);
 
   useEffect(() => {
     setJobs(data);
   }, []);
+
+  const filterFunc = ({ role, level, tools, languages }) => {
+    if (filters.length === 0) {
+      return true;
+    }
+
+    const tags = [role, level];
+
+    if (tools) {
+      tags.push(...tools);
+    }
+
+    if (languages) {
+      tags.push(...languages);
+    }
+
+    return tags.some((tag) => filters.includes(tag));
+  };
+
+  const handleTagClick = (tag) => {
+    setFilters([...filters, tag]);
+  };
+
+  const handleFilterClick = (passedFilter) => {
+    setFilters(filters.filter((f) => f !== passedFilter));
+  };
+
+  const filteredJobs = jobs.filter(filterFunc);
 
   return (
     <div className="App">
       <header className="bg-teal-500 mb-12">
         <img src="/images/bg-header-desktop.svg" alt="" />
       </header>
+      <div className="flex bg-white shadow-md my-16 mx-10 p-6 rounded">
+        {filters.length > 0 &&
+          filters.map((filter) => (
+            <span
+              onClick={() => handleFilterClick(filter)}
+              className="cursor-pointer text-teal-500 bg-teal-100 font-bold mr-4 mb-4 p-2 rounded sm:mb-0"
+            >
+              {filter}
+            </span>
+          ))}
+      </div>
       {jobs.length === 0 ? (
         <p>Jobs are fetching...</p>
       ) : (
-        jobs.map((job) => <JobBoardComponent job={job} key={job.id} />)
+        filteredJobs.map((job) => (
+          <JobBoardComponent
+            job={job}
+            key={job.id}
+            handleTagClick={handleTagClick}
+          />
+        ))
       )}
     </div>
   );
--- src/components/JobBoardComponent.jsx
+++ src/components/JobBoardComponent.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function JobBoardComponent({ job }) {
+export default function JobBoardComponent({ job, handleTagClick }) {
   const tags = [job.role, job.level];
 
   if (job.tools) {
@@ -46,7 +46,10 @@ export default function JobBoardComponent({ job }) {
       <div className="flex flex-wrap items-center mt-4 mx-4 pt-4 border-t border-gray-500 border-solid sm:ml-auto sm:border-0 sm:pt-0 sm:mt-0 sm:justify-center sm:pt-0 sm:mt-0">
         {tags
           ? tags.map((tag) => (
-              <span className="text-teal-500 bg-teal-100 font-bold mr-4 mb-4 p-2 rounded">
+              <span
+                onClick={() => handleTagClick(tag)}
+                className="cursor-pointer text-teal-500 bg-teal-100 font-bold mr-4 mb-4 p-2 rounded"
+              >
                 {tag}
               </span>
             ))
